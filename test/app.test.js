b0e0ced6d9b4ec9190833fd99dbf2b50b743150e
kittybagpipes <sparticus978@gmail.com>
2020-11-29 14:16:32 -0800

    added full coverage test suite

--- test/app.test.js
+++ test/app.test.js
@@ -29,4 +29,70 @@ describe("GET/apps", () => {
         );
       });
   });
+
+  it("returns 400 when sorting query is incorrect", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ sort: "GOOFED" })
+      .expect(400, "Must be sorted by rating or app name, nothing else");
+  });
+
+  it("returns 400 when genre selected is NOT a valid query", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ genre: "GOOFED" })
+      .expect(
+        400,
+        "Not a listed genre, please use: Action, Puzzle, Strategy, Casual, Arcade, or Card"
+      );
+  });
+  it("should sort by app name", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ sort: "App" })
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then((res) => {
+        expect(res.body).to.be.an("array");
+        let sorted = true;
+
+        let i = 0;
+
+        while (i < res.body.length - 1) {
+          const appAtI = res.body[i];
+          const appAtIPlus1 = res.body[i + 1];
+          if (appAtIPlus1.App < appAtI.App) {
+            sorted = false;
+            break;
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+  it("should sort by app rating", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ sort: "Rating" })
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then((res) => {
+        expect(res.body).to.be.an("array");
+        let sorted = true;
+
+        let i = 0;
+
+        while (i < res.body.length - 1) {
+          const ratingAtI = res.body[i];
+          const ratingAtIPlus1 = res.body[i + 1];
+          if (Number(ratingAtIPlus1.Rating) > Number(ratingAtI.Rating)) {
+            sorted = false;
+            break;
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
 });
